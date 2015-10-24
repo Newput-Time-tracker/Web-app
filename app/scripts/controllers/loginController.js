2a29766f0eddbe800505165fbbbaef9befb12473
varshatyagi <newput@Newputs-MacBook.local>
2015-10-24 15:48:35 +0530

    api integration

--- app/scripts/controllers/loginController.js
+++ app/scripts/controllers/loginController.js
@@ -1,6 +1,11 @@
 app.controller('loginController', ['$scope', '$location', 'userServices', function($scope, $location, userServices){
 	this.verifyUser = function() {
-		$scope.employees = userServices.authUser();
+		var employeesPromise = userServices.authUser($scope.user);
+		employeesPromise.then(function(res){
+				$scope.employees = res;
+			}, function(error){
+				console.log(error)
+		});
 		$location.path('/usertimesheet');
 	};
 	this.toLocation = function(loc){
