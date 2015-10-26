6ad34439993e70b1620a01275de14cc0d5c72d96
varshatyagi <newput@Newputs-MacBook.local>
2015-10-26 15:18:50 +0530

    structured the gulp file with required plughins

--- gulpfile.js
+++ gulpfile.js
@@ -5,83 +5,87 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
-var path = 'app/assets/';
-var buildCssPath = 'build/css';
-var buildJsPath = 'build/js';
-var components = 'bower_components/' ; 
-var pathArr = [components+'jquery/dist/jquery.min.js', 
-components+'bootstrap/dist/js/bootstrap.min.js', 
-components+'angular-route/angular-route.min.js', components+'angular/angular.min.js'];
-var appjs = 'app/scripts/';
 
-// Compile, concat, and minify Our Sass
+// initialize variables.
 
-gulp.task('minifycss', function() {
-  return gulp.src(path+'styles/**/*.scss')
-  .pipe(concat('.min.css'))
-	  .pipe(minifyCss({
-		  	relativeTo: '../bower_components',
-	    processImport: true
-	  }))
-	  .pipe(gulp.dest(buildCssPath));
+var buildCssPath = 'build/assets/styles';
+var buildJsPath = 'build/assets/js';
+var vendorJsArr = ['bower_components/jquery/dist/jquery.min.js', 
+'bower_components/bootstrap/dist/js/bootstrap.min.js', 
+'bower_components/angular-route/angular-route.min.js',
+'bower_components/angular/angular.min.js', 'bower_components/angular-cookies/angular-cookies.min.js', 
+'bower_components/ngMask/dist/ngMask.min.js'];
+
+var applicationJsArr = ['app/scripts/app.js', 
+'app/scripts/services/userService.js',
+'app/scripts/controllers/loginController.js',
+'app/scripts/controllers/signUpController.js',
+'app/scripts/controllers/userTimesheetController.js',
+'app/scripts/controllers/detalViewController.js',
+'app/scripts/controllers/verifyController.js'
+];
+
+var vendorCss = ['bower_components/bootstrap/dist/css/bootstrap.min.css', 
+'bower_components/open-sans-fontface/open-sans.css'];
+
+// Task only for development environment
+gulp.task('generateDevCss', function() {
+  return gulp.src('app/assets/styles/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/assets/styles'));
 });
 
-
-gulp.task('tempTask', function() {
-  return gulp.src(path+'styles/**/*.scss')
-  .pipe(sass())
-  .pipe(gulp.dest(path+'styles'));
-});
-
+//Compress vendor css
 gulp.task('compressVendorCss', function() {
-  return gulp.src(components+'bootstrap/dist/css/bootstrap.min.css')
-  .pipe(rename('vendor.min.css'))
-  .pipe(gulp.dest(buildCssPath));
+  return gulp.src(vendorCss)
+    .pipe(minifyCss())
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest(buildCssPath));
 });
 
-// Compile, concat, and minify Our js
-
+// Compile, concat, and minify vendor js files
 gulp.task('minifyjs', function() {
-  return gulp.src(pathArr)
-	  .pipe(uglify())
-	  .pipe(concat('vendor.js'))
-	  .pipe(gulp.dest(buildJsPath));
+  return gulp.src(vendorJsArr)
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest(buildJsPath));
 });
 
-//compile appication js
-
+//compile, concat and minify appication js files
 gulp.task('applicationjs', function() {
-	return gulp.src(appjs+'app.js')
-	 .pipe(addsrc.append(appjs+'services/*.js'))
-	 .pipe(addsrc.append(appjs+'controllers/*.js'))
-	 .pipe(uglify())
-	 .pipe(concat('application.js'))
-	 .pipe(gulp.dest(build+'js'));
+  return gulp.src(applicationJsArr)
+   .pipe(uglify())
+   .pipe(concat('application.js'))
+   .pipe(gulp.dest(buildJsPath));
 });
 
 // copy images .pipe(changed(imgDst))
 gulp.task('copyimage', function() {
-  var imgSrc = path+'images/**/*',
-      imgDst = build+'images';
-  gulp.src(imgSrc)
-    //.pipe(imagemin())
-    .pipe(gulp.dest(imgDst));
+  return gulp.src('app/assets/images/*.{png, jpg, jpeg}')
+    .pipe(gulp.dest('build/assets/images'));
 });
 
-//copy index.html page
-
-gulp.task('copyindexpage', function() {
-   return gulp.src('app/*.html')
-     //.pipe(minifyHTML())
-     .pipe(gulp.dest('build'));
-	
+//copy index.html pages
+gulp.task('copyHtmlPage', function() {
+  return gulp.src('app/*.html')
+    .pipe(gulp.dest('build'));
 });
 
-gulp.task('copyviewfiles', function() {
-   return gulp.src('app/views/*.html')
-     //.pipe(minifyHTML())
-     .pipe(gulp.dest(build+'views'));
-	
+//copy partials to build
+gulp.task('copyPartials', function() {
+  return gulp.src('app/views/*.html')
+    .pipe(gulp.dest('build/views'));
 });
+
+//copy favicon.ico
+gulp.task('favicon', function() {
+  return gulp.src('app/*.ico')
+    .pipe(gulp.dest('build'));
+});
+
+// Compile, concat, and minify Our Sass
+gulp.task('minifycss', function() {
+  return gulp.src('app/assets/styles/*.css')
+    .pipe(minifyCss())
+    .pipe(gulp.dest(buildCssPath));
+ });
