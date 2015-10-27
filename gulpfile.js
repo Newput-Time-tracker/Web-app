1d95e44695d0e56031521a4d971ae122b30e314a
Shashank saxena <shashank@newput.com>
2015-10-28 03:27:56 +0530

    project configuration changes to deploy on heroku

--- gulpfile.js
+++ gulpfile.js
@@ -1,99 +1,105 @@
-// Include gulp
+// include node modules
 var gulp = require('gulp');
-
-// Include Our Plugins
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var minifyCss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 
-// initialize variables.
+// vendor files
+var vendorCSS = [
+  'bower_components/open-sans-fontface/open-sans.css',
+  'bower_components/bootstrap/dist/css/bootstrap.min.css'
+];
 
-var buildCssPath = 'build/assets/styles';
-var buildJsPath = 'build/assets/js';
-var vendorJsArr = ['bower_components/jquery/dist/jquery.min.js',
-'bower_components/bootstrap/dist/js/bootstrap.min.js',
-'bower_components/angular-route/angular-route.min.js',
-'bower_components/angular/angular.min.js', 'bower_components/angular-cookies/angular-cookies.min.js',
-'bower_components/ngMask/dist/ngMask.min.js'];
+var vendorScripts = [
+  'bower_components/jquery/dist/jquery.min.js',
+  'bower_components/bootstrap/dist/js/bootstrap.min.js',
+  'bower_components/angular/angular.min.js',
+  'bower_components/angular-route/angular-route.min.js',
+  'bower_components/angular-cookies/angular-cookies.min.js',
+  'bower_components/ngMask/dist/ngMask.min.js',
+];
 
-var applicationJsArr = ['app/scripts/app.js',
-'app/scripts/services/userService.js',
-'app/scripts/controllers/loginController.js',
-'app/scripts/controllers/signUpController.js',
-'app/scripts/controllers/userTimesheetController.js',
-'app/scripts/controllers/detalViewController.js',
-'app/scripts/controllers/verifyController.js'
+// application files
+var applicationScripts = [
+  'app/scripts/app.js',
+  'app/scripts/services/userService.js',
+  'app/scripts/controllers/loginController.js',
+  'app/scripts/controllers/signUpController.js',
+  'app/scripts/controllers/userTimesheetController.js',
+  'app/scripts/controllers/detalViewController.js',
+  'app/scripts/controllers/verifyController.js'
 ];
 
 var srcArr = ['app/index.html', 'app/favicon.ico', 'app/404.html'];
 
-var vendorCss = ['bower_components/bootstrap/dist/css/bootstrap.min.css',
-'bower_components/open-sans-fontface/open-sans.css'];
+// build assets
+var buildCssPath = 'build/assets/styles';
+var buildJsPath = 'build/assets/js';
 
-// Task only for development environment
-gulp.task('generateDevCss', function() {
+// compile all SCSS files into their CSS versions
+gulp.task('compileStyles', function() {
   return gulp.src('app/assets/styles/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('app/assets/styles'));
 });
 
-// Compile, concat, and minify Our Sass
-gulp.task('applicationCss', function() {
+// minify all CSS files and merge into single minified CSS file
+gulp.task('prepareAppStyles', function() {
   return gulp.src('app/assets/styles/*.css')
     .pipe(minifyCss())
     .pipe(concat('application.min.css'))
     .pipe(gulp.dest(buildCssPath));
  });
 
-//Compress vendor css
-gulp.task('compressVendorCss', function() {
-  return gulp.src(vendorCss)
-    .pipe(concat('vendor.min.css'))
-    .pipe(gulp.dest(buildCssPath));
-});
-
-// Compile, concat, and minify vendor js files
-gulp.task('compressVendorJs', function() {
-  return gulp.src(vendorJsArr)
-    .pipe(concat('vendor.min.js'))
-    .pipe(gulp.dest(buildJsPath));
-});
-
-//compile, concat and minify appication js files
-gulp.task('applicationJs', function() {
-  return gulp.src(applicationJsArr)
+// minify and merge all app JS into single JS file
+gulp.task('prepareAppScripts', function() {
+  return gulp.src(applicationScripts)
    .pipe(uglify())
    .pipe(concat('application.min.js'))
    .pipe(gulp.dest(buildJsPath));
 });
 
-// copy images .pipe(changed(imgDst))
-gulp.task('copyimage', function() {
+// merge all vendor CSS into single CSS file
+gulp.task('prepareVendorStyles', function() {
+  return gulp.src(vendorCSS)
+    .pipe(concat('vendor.min.css'))
+    .pipe(gulp.dest(buildCssPath));
+});
+
+// merge all vendor JS into single JS file
+gulp.task('prepareVendorScripts', function() {
+  return gulp.src(vendorScripts)
+    .pipe(concat('vendor.min.js'))
+    .pipe(gulp.dest(buildJsPath));
+});
+
+// copy images into build directory
+gulp.task('copyImage', function() {
   return gulp.src('app/assets/images/*.{png, jpg, jpeg}')
     .pipe(gulp.dest('build/assets/images'));
 });
 
-//copy partials to build
+//copy partials into build directory
 gulp.task('copyPartials', function() {
   return gulp.src('app/views/*.html')
     .pipe(gulp.dest('build/views'));
 });
 
-//copy favicon.ico and index.html
-gulp.task('movedSource', function() {
+//copy app level files into build directory
+gulp.task('copyFiles', function() {
   return gulp.src(srcArr)
     .pipe(gulp.dest('build'));
 });
 
-// default task to move the files
-gulp.task('moveFiles', ['copyimage', 'copyPartials', 'movedSource']);
+// task to copy all files into build directory
+gulp.task('copyAppFiles', ['copyImage', 'copyPartials', 'copyFiles']);
 
-// default task to compress , concat the vendor files
-gulp.task('vendorFiles', ['compressVendorCss', 'compressVendorJs']);
+// task to prepare all vendor assets
+gulp.task('prepareVendorAssets', ['prepareVendorStyles', 'prepareVendorScripts']);
 
-// default task to compress , concat the application files
-gulp.task('applicationrFiles', ['generateDevCss', 'applicationCss', 'applicationJs']);
+// task to preapre all app level assets
+gulp.task('prepareAppAssets', ['compileStyles', 'prepareAppStyles', 'prepareAppScripts']);
 
-// default task to run
-gulp.task('defaultRun', ['moveFiles', 'vendorFiles', 'applicationrFiles']);
+// task to build the app
+gulp.task('buildApp', ['copyAppFiles', 'prepareVendorAssets', 'prepareAppAssets']);
