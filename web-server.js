9e697c390ad6c3c2c5ab12ea06aa505ca2d5371f
Shashank saxena <shashank@newput.com>
2015-10-30 16:43:49 +0530

    port changes

--- web-server.js
+++ web-server.js
@@ -26,7 +26,7 @@ app.get('/', function(req, res) {
   });
 });
 
-var server = app.listen(8080, function () {
+var server = app.listen((process.env.PORT || 5000), function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log(__dirname);
