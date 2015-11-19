66fd94532592893703c991eca0377e5c15a73f99
varshatyagi <newput@Newputs-MacBook.local>
2015-11-19 09:47:49 +0530

    updation in dropdown option

--- app/scripts/services/httpRequest.js
+++ app/scripts/services/httpRequest.js
@@ -1,14 +1,7 @@
 /* global app: false */
 
 app.factory('HttpRequest', ['$rootScope', 'CONFIG', function($rootScope, CONFIG) {
-  // var authToken = 'FB3AA4D6F393D0AEAEC5B3F65DBE3463';
   return {
-    // 'request': function(config) {
-      // config.headers['token'] = authToken;
-      // config.headers['empId'] = 12;
-      // return config;
-    // },
-
     'responseError': function(rejection) {
       if (rejection.status == CONFIG.MISSING_PARAMETER || rejection.status == CONFIG.UNAUTHORIZE_ACCESS) {
         $rootScope.$broadcast("authFailure");
